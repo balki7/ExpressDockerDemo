19d4338e3ea307c33dfb2a5ee4290d2ac0b63669
Balkı <balki1985@gmail.com>
2020-04-28 11:30:16 +0300

    Initial

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const app = require('./app.js');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
